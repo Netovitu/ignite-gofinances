9516203814468a6bfc6f97d6933b4c94f632b578
Neto <netovitu@gmail.com>
2022-06-14 16:20:48 -0300

    Adicionando listagem de entradas e saidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { HighlightCard } from '../../Components/HighlightCard';
+import { TransactionCard, TransactionCardProps } from '../../Components/TransactionCard';
 
 import { 
   Container,
@@ -13,10 +15,51 @@ import {
   Icon,
   HighlightCards,
   Transactions,
-  Title
+  Title,
+  TransactionList
  } from './styles'
 
+export interface DataListProps extends TransactionCardProps{
+  id: string;
+ }
+
 export function Dashboard(){
+  const data = [
+    {
+      id: '1',
+      type: 'positive',
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.00,00",
+      category: {
+        name: 'Vendas',
+        icon: 'dollar-sign',
+      },
+      date: "13/04/2022"
+    },
+    {
+      id: '2',
+      type: 'negative',
+      title: "Hamburgueria Pizzy",
+      amount: "R$ 59,00",
+      category: {
+        name: 'Alimentação',
+        icon: 'coffee',
+      },
+      date: "13/04/2022"
+    },
+    {
+      id: '3',
+      type: 'negative',
+      title: "Aluguel do apartamento",
+      amount: "R$ 1.200,00",
+      category: {
+        name: 'Casa',
+        icon: 'shopping-bag',
+      },
+      date: "13/04/2022"
+    },
+  
+  ];
   return (
     <Container>
       <Header>
@@ -57,9 +100,13 @@ export function Dashboard(){
       </HighlightCards>
 
       <Transactions>
-        <Title>
+        <Title>Listagem</Title>
 
-        </Title>
+        <TransactionList 
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
       </Transactions>
     </Container>
   )
